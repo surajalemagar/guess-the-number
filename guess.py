f87afa56d9e530f5531d2d629c8347d890542af9
suraj <surajido111@gmail.com>
2020-04-23 07:48:02 +0545

    guess the number game is completed using python!!

--- guess.py
+++ guess.py
@@ -1,3 +1,22 @@
 import random
-num1 = random.randint(0,20)
-print("Random integer: ", num1)
+n=random.randint(0,20)
+print("GUESS THE NUMBER!!!")
+N=int(input("\nEnter the number of wrong attempt!!!:"))
+while N>0:
+    print("Number of attempt remaining:",N)
+    guess=int(input("Guess the number between 1 and 20:"))
+    if guess==n:
+        print("\nCorrect Answer is :",n)
+        print("Congratulations!!! You Guess the right number!!!")
+        break
+    else:
+        N=N-1
+        if N==0:
+            print("\nYou lose the game.Try Again!!!")
+            print("Correct Answer is :",n)
+            break
+        if guess>n:
+            print("\nYou need to guess lower:")
+        else:
+            print("\nYou need to guess higher:")
+    
